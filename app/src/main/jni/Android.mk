33f32f70c6011d51417865600a5f27393f33bb1b
Philippe Chepy <philippe@chepy.eu>
2014-09-17 23:02:49 +0200

    Updated: (jni) necessary jni libs for deezer api.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,3 +4,4 @@ include /home/phil/AndroidStudioProjects/AudioKit/app/src/main/jni/prebuilt-soxr
 include /home/phil/AndroidStudioProjects/AudioKit/app/src/main/jni/prebuilt-ffmpeg/Android.mk
 include /home/phil/AndroidStudioProjects/AudioKit/app/src/main/jni/prebuilt-taglib/Android.mk
 include /home/phil/AndroidStudioProjects/AudioKit/app/src/main/jni/medialib/Android.mk
+include /home/phil/AndroidStudioProjects/AudioKit/app/src/main/jni/prebuilt-deezer/Android.mk
--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a armeabi x86 mips
 
-APP_MODULES := medialib
+APP_MODULES := medialib deezer
 APP_PLATFORM := android-8
 
 APP_STL := stlport_static
